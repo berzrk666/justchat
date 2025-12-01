9b3384fa02d5f5b36cce1b6959c97e3831a7f547
bzrak <67083456+bzrak@users.noreply.github.com>
2025-12-01 18:08:22 -0300

    fix: set up username as unique in db table

--- server/src/chat_server/db/models.py
+++ server/src/chat_server/db/models.py
@@ -12,5 +12,5 @@ class UserTable(Base):
     __tablename__ = "user"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    username: Mapped[str] = mapped_column(String(USERNAME_MAX_LENGTH))
-    hashed_password: Mapped[str] = mapped_column(String())
+    username: Mapped[str] = mapped_column(String(USERNAME_MAX_LENGTH), unique=True)
+    hashed_password: Mapped[str] = mapped_column(String(255))
